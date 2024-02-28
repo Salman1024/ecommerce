8bfacf01a072271a60a1aacb19c43bc1c22ef78c
Salman1024 <sm4777515gmail.com>
2024-02-28 17:36:30 +0500

    bit style change

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -32,14 +32,14 @@ const Login = () => {
                 />
                 
               </div>
-              <div>
+              <div className="">
                 <a href="#" className=" text-blue-700">
                   {" "}
                   Forget Password?
                 </a>
                 
               </div>
-              <div className="sign-btn ">
+              <div className="sign-btn my-2">
                 <button className="bg-green-500 w-full py-2 px-2 text-white rounded-lg">Sign In</button>
               </div>
               <div className="line flex items-center  justify-center mb-2">
@@ -48,7 +48,7 @@ const Login = () => {
                 <div className="w-1/2 border-t-2 ml-1 border-black"></div>
               </div>
 
-              <div className="login-other flex flex-col items-center justify-center gap-2">
+              <div className="login-other flex flex-col items-center justify-center gap-3">
 
                 <button className="google-btn flex items-center justify-center border border-gray-300 w-full overflow-x-hidden p-2 rounded-md">
                   <GoogleIcon />
