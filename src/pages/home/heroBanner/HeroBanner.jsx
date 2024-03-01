9748644c9c8985866fd91a5e15d41efbcdd11977
Salman1024 <sm4777515@gmail.com>
2024-03-01 21:17:39 +0500

    slider setting

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,15 +1,32 @@
-import React from "react";
-import SkinCare from "../../../assets/skin care.jpg";
+import React, { useEffect, useState } from "react";
+import toy from '../../../assets/toy.jpg'
+import living from '../../../assets/living.jpg'
+import dog from '../../../assets/dog.jpg'
 
 
 const HeroBanner = () => {
+
+  const images=[toy,living,dog];
+
+  const [currentImage,setCurrentImage]=useState(0);
+  const length=images.length;
+
+  useEffect(()=>{
+    const interval=setInterval(()=>
+    {
+      setCurrentImage((prevIndex) =>(prevIndex === length -1 ? 0 : prevIndex + 1)   )
+    },5000);
+    return ()=> clearInterval(interval)
+  },[])
+
+
   return (
-    <div className="heroBanner">
-      <div className="dropImage ">
-        <img className="h-full max-h-[70vh] w-full object-cover object-center" src={SkinCare} alt="" />
+    <>
+      <div className="banner ">
+        <img src={images[currentImage]} alt="" />
+        <p>Hero Banner</p>
       </div>
-        <p>Hello banner</p>
-    </div>
+    </>
   );
 };
 
