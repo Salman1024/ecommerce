3a2e4ad190dd790ccf87536182eb25434e30364d
Salman1024 <sm4777515gmail.com>
2024-03-15 20:59:34 +0500

    navbar change

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -4,18 +4,17 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <header className="header flex items-center justify-between h-[70px] w-full px-2 bg-light-black">
-        
-          <Link to="/" className="logo-name text-2xl font-semibold mr-4 ">
-            MegaMart
-          </Link>
-       
-        <div>
-          <ul className="links">
-            <a href="#">Login</a>
+      <div className="navbar flex items-center w-full bg-light-black h-[70px] ">
+        <div className="header flex items-center justify-between w-full max-w-[1200px] m-auto px-10">
+          <div className="navbar-logo mr-1">
+            <span className="text-2xl font-semibold">STORE</span>
+          </div>
+          <ul className="navbar-links flex gap-4">
+            <li>Login</li>
+            <li>About</li>
           </ul>
         </div>
-      </header>
+      </div>
     </>
   );
 };
